cd6fd88c6c527d2cbff1e34a5192bdfd13c1e3c4
magno.franc <magno.franc@awatapu.school.nz>
2021-07-05 12:05:13 +1200

    Multiply by 5

--- venv/00_sandbox.py
+++ venv/00_sandbox.py
@@ -1,8 +1,8 @@
 #Ask user for number
-number = input("Give thy a number!")
+number = int(input("Give thy a number!"))
 
 #Multiply number by 5
 by_five = number * 5
 
 #Print the outcome
-print(by_five)
+print("{} is equal to {} times 5".format(by_five, number))
