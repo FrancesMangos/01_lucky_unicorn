34f84282d5cf6f048cfd5f163fc7405d79857c93
magno.franc <magno.franc@awatapu.school.nz>
2021-07-05 12:02:21 +1200

    Multiply by 5

--- venv/00_sandbox.py
+++ venv/00_sandbox.py
@@ -1,2 +1,7 @@
-#Comment on the comment with a comment
-print("holy jesus christ have i done it??????")
+#Ask user for number
+number = input("Give thy a number!")
+
+#Multiply number by 5
+by_five = number * 5
+
+print(by_five)
