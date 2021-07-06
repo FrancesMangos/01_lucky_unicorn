3d6864c8db0211598da8e3231d18154ba9480245
magno.franc <magno.franc@awatapu.school.nz>
2021-07-06 12:56:27 +1200

    Yes/no checker version one

---
+++ venv/00_yes_no_checker.py
@@ -0,0 +1,21 @@
+# Ask user if they have played the game before
+show_instructions = input("Have thou played the"
+                          " game before?").lower()
+
+# If input yes, continue program
+if show_instructions == "yes":
+    print("thy program continues")
+
+elif show_instructions == "y":
+    print("thy program continues")
+
+# If input no, display instructions
+elif show_instructions == "no":
+    print("display thy instructions")
+
+elif show_instructions == "n":
+    print("display thy instructions")
+
+#If input not yes or no, clarify question
+else:
+    print("please insert a yes / no")
