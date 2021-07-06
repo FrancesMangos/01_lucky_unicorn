96cf2389561bc631d6ed37aa62c2c5e274346793
magno.franc <magno.franc@awatapu.school.nz>
2021-07-06 13:25:18 +1200

    Yes/no checker but two versions in one

--- venv/00_yes_no_checker_v2.py
+++ venv/00_yes_no_checker_v2.py
@@ -1,3 +1,24 @@
+#Functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input("Have thou played the game before?").lower()
+
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        # If input no, display instructions
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        #If input not yes or no, clarify question
+        else:
+            print("<error> please insert a yes / no")
+
+#Main code goes here...
 show_instructions = ""
 while show_instructions.lower() != "xxx":
     # Ask user if they have played the game before
