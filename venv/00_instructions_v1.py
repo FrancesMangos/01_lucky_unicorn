a60f121dd0bd38b037824b3bd573e7b79956c8cd
magno.franc <magno.franc@awatapu.school.nz>
2021-07-07 14:21:51 +1200

    It don't know if this works but I am moving on regardless

---
+++ venv/00_instructions_v1.py
@@ -0,0 +1,29 @@
+# Functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input("Have thou played "
+                         "the game before?").lower()
+
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        # If input no, display instructions
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        #If input not yes or no, clarify question
+        else:
+            print("<error> please insert a yes / no")
+
+# Main code goes here...
+show_instructions = yes_no("Have thou played "
+                         "the game before?")
+print("You chose {}".format(show_instructions))
+print()
+
+having_fun = yes_no("Are thou having fun?")
+print("You said {} to having fun".format(having_fun))
