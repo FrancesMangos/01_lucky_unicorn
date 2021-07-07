3e203a7b2a1fdb1258f0fd271ce0dfef4f6648fa
magno.franc <magno.franc@awatapu.school.nz>
2021-07-07 14:15:40 +1200

    Completed yes/no checker version two

--- venv/00_yes_no_checker_v2.py
+++ venv/00_yes_no_checker_v2.py
@@ -1,8 +1,9 @@
-#Functions go here...
+# Functions go here...
 def yes_no(question):
     valid = False
     while not valid:
-        response = input("Have thou played the game before?").lower()
+        response = input("Have thou played "
+                         "the game before?").lower()
 
 
         if response == "yes" or response == "y":
@@ -18,25 +19,7 @@ def yes_no(question):
         else:
             print("<error> please insert a yes / no")
 
-#Main code goes here...
-show_instructions = ""
-while show_instructions.lower() != "xxx":
-    # Ask user if they have played the game before
-    show_instructions = input("Have thou played the game before?").lower()
-
-    # If input yes, continue program
-    if show_instructions == "yes" or show_instructions == "y":
-        show_instructions = "yes"
-        print("thy program continues")
-
-    # If input no, display instructions
-    elif show_instructions == "no"or show_instructions == "n":
-        show_instructions = "no"
-        print("display thy instructions")
-
-    #If input not yes or no, clarify question
-    else:
-        print("<error> please insert a yes / no")
-
-
-    print("You chose {}".format(show_instructions))
+# Main code goes here...
+show_instructions = yes_no("Have thou played "
+                         "the game before?").lower()
+print("You chose {}".format(show_instructions))
