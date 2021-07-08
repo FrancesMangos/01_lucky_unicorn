5661e061e54f8a81f75fdc1b4c365d2576014603
magno.franc <magno.franc@awatapu.school.nz>
2021-07-09 09:51:44 +1200

    Version one of how_much without video

---
+++ venv/04_how_much_v1.py
@@ -0,0 +1,14 @@
+error = "Please enter a whole number between 1 and 10"
+
+valid = False
+while not valid:
+
+    response = int(input("How much money would you like to play with?"))
+
+
+    if response == < 1 or > 10
+    print("Please enter a number between 1 and 10")
+    return response
+
+    else:
+    print(error)
