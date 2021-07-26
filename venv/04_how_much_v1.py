a5065f608e8e946c9d246537d98e9ac5ea5290e1
magno.franc <magno.franc@awatapu.school.nz>
2021-07-26 12:55:26 +1200

    All incorrect inputs output the same response

--- venv/04_how_much_v1.py
+++ venv/04_how_much_v1.py
@@ -1,16 +1,19 @@
-error = "Please enter a whole number between 1 and 10"
+error = "Please enter a whole number between 1 and 10\n"
 
 valid = False
 while not valid:
-    #ask the question
-    response = int(input("How much money would you like to play with?"))
+    try:
+        #ask the question
+        response = int(input("How much money would you "
+                             "like to play with?" ))
 
-    #if the amount is too low or too high
-    if 0 < response <= 10:
-        print("You have asked to"
-              " play with ${}".format(response))
+        #if the amount is too low or too high
+        if 0 < response <= 10:
+            print("You have asked to"
+                  " play with ${}".format(response))
 
-    #output an error
-    else:
-        print(error)
+        #output an error
+        else:
+            print(error)
 
+    except ValueError
