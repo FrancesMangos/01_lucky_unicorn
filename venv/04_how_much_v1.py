2b3b549c1978d351f8cea36c836ed98a5e040a67
magno.franc <magno.franc@awatapu.school.nz>
2021-07-26 12:52:43 +1200

    Added comments

--- venv/04_how_much_v1.py
+++ venv/04_how_much_v1.py
@@ -2,13 +2,15 @@ error = "Please enter a whole number between 1 and 10"
 
 valid = False
 while not valid:
-
+    #ask the question
     response = int(input("How much money would you like to play with?"))
 
-
+    #if the amount is too low or too high
     if 0 < response <= 10:
+        print("You have asked to"
+              " play with ${}".format(response))
 
-        print("You have asked to play with ${}".format(response))
-
+    #output an error
     else:
         print(error)
+
