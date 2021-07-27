3079851f96a841a0baf6152c855ddce8287f7ff4
magno.franc <magno.franc@awatapu.school.nz>
2021-07-27 15:04:09 +1200

    token gen v1

---
+++ venv/06_token_generator_v1.py
@@ -0,0 +1,21 @@
+import random
+
+#Main routine goes here...
+tokens = ["unicorn", "horse", "zebra", "donkey"]
+balance = 100
+
+#Testing loop to generate 20 tokens
+for item in range(0, 20):
+    chosen = random.choice(tokens)
+    print(chosen, end='\t')
+
+    #Adjust balance
+    if chosen == "unicorn":
+        balance += 4
+    elif chosen == "donkey":
+        balance -= 1
+    else:
+        balance -= 0.5
+
+    #output
+    print("Tokens: {}, Balance: {}".format(chosen, balance))
