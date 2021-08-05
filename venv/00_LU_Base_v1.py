601fdb0708474f8ab579503ffc3897e049d8af75
magno.franc <magno.franc@awatapu.school.nz>
2021-08-06 09:53:28 +1200

    adding token generation and looping rounds

--- venv/00_LU_Base_v1.py
+++ venv/00_LU_Base_v1.py
@@ -1,3 +1,5 @@
+import random
+
 #Functions go here
 def num_check(question, low, high):
     error = "Please enter a whole number between 1 and 10\n"
@@ -59,4 +61,43 @@ else:
 how_much = num_check("How much money would you "
                              "like to play with?", 0, 10 )
 
-print("You will be spending ${}".format(how_much))
+balance = how_much
+
+rounds_played = 0
+
+play_again = input("Press <Enter> to play...").lower()
+while play_again == "":
+
+    #increase # of rounds played
+    rounds_played += 1
+
+    #print round number
+    print()
+    print("*** Round #{} ***".format(rounds_played))
+
+    chosen_num = random.randint(1, 100)
+
+    #Adjust balance
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+    else:
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+
+    print("You got a {}. Your balance is ${:.2f}".format(chosen, balance))
+    #If balance is low,exit the game and output suitable message
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+        play_again = input("Press Enter to play again or 'xxx' to quit")
+
+print()
+print("Final Balance", balance)
